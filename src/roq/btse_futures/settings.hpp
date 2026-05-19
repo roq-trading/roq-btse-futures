28f7bda3d962c4e1b9c89361c84802d941d8973a
thraneh <thraneh@gmail.com>
2026-05-19 15:28:34 +0000

    move gateway settings to flags

--- src/roq/btse_futures/settings.hpp
+++ src/roq/btse_futures/settings.hpp
@@ -2,45 +2,12 @@
 
 #pragma once
 
-#include <fmt/format.h>
-
-#include "roq/server/flags/settings.hpp"
-
-#include "roq/btse_futures/flags/flags.hpp"
-#include "roq/btse_futures/flags/misc.hpp"
-#include "roq/btse_futures/flags/rest.hpp"
-#include "roq/btse_futures/flags/ws.hpp"
+#include "roq/btse_futures/flags/settings.hpp"
 
 namespace roq {
 namespace btse_futures {
 
-struct Settings final : public server::flags::Settings, public flags::Flags {
-  explicit Settings(args::Parser const &);
-
-  flags::Misc misc;
-  flags::REST rest;
-  flags::WS ws;
-};
+using Settings = flags::Settings;
 
 }  // namespace btse_futures
 }  // namespace roq
-
-template <>
-struct fmt::formatter<roq::btse_futures::Settings> {
-  constexpr auto parse(format_parse_context &context) { return std::begin(context); }
-  auto format(roq::btse_futures::Settings const &value, format_context &context) const {
-    using namespace std::literals;
-    return fmt::format_to(
-        context.out(),
-        R"({{)"
-        R"(misc={}, )"
-        R"(rest={}, )"
-        R"(ws={}, )"
-        R"(server={})"
-        R"(}})"sv,
-        value.misc,
-        value.rest,
-        value.ws,
-        static_cast<roq::server::Settings const &>(value));
-  }
-};
