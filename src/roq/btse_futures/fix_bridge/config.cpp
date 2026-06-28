a7435e6e73a4734d347c0dd6b94e04aa28e06403
thraneh <thraneh@gmail.com>
2026-06-28 02:35:16 +0000

    moving fix_bridge into roq-server

---
+++ src/roq/btse_futures/fix_bridge/config.cpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#include "roq/btse_futures/fix_bridge/config.hpp"
+
+using namespace std::literals;
+
+namespace roq {
+namespace btse_futures {
+namespace fix_bridge {
+
+// === IMPLEMENTATION ===
+
+}  // namespace fix_bridge
+}  // namespace btse_futures
+}  // namespace roq
---
+++ src/roq/btse_futures/fix_bridge/live.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+CWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+if [ "$1" == "debug" ]; then
+  PREFIX="gdb --args"
+else
+  PREFIX=
+fi
+
+NAME="btse-futures"
+
+CONFIG="${CONFIG:-$NAME}"
+
+CONFIG_FILE="$ROQ_CONFIG_PATH/roq-btse-futures/$CONFIG.toml"
+
+FLAGFILE="../../../../share/flags/prod/flags.cfg"
+
+$PREFIX ./roq-btse-futures-fix-bridge \
+  --name "$NAME" \
+  --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
+  --cache_dir "$HOME/var/lib/roq/cache" \
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --client_listen_address "$HOME/run/$NAME.sock" \
+  --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
+  $@
