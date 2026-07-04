512a2a9f50c3017968d955a4db69c1e11a4bc819
thraneh <thraneh@gmail.com>
2026-07-04 03:18:56 +0000

    preparing roq-*-proto-bridge

---
+++ src/roq/btse_futures/proto_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/btse_futures/gateway/config.hpp"
+
+namespace roq {
+namespace btse_futures {
+namespace proto_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace proto_bridge
+}  // namespace btse_futures
+}  // namespace roq
